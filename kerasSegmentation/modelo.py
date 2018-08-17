fd7ec33581dc5e3266a93627ff25f119a88aaf49
Emanuel <ealvarado97@gmail.com>
2018-08-17 15:14:47 -0600

    Agregación de javadoc

--- kerasSegmentation/modelo.py
+++ kerasSegmentation/modelo.py
@@ -4,7 +4,7 @@ from keras.layers.core import Dense
 from keras.models import model_from_json
 from keras.models import load_model
 
-model =None
+model =None #variable donde se va a cargar el modelo .h5
 
 def main():
 
@@ -12,7 +12,10 @@ def main():
     model= cargar_modelo('best_female_model.h5')
     model=compiler_modelo(model)
 
-
+#
+#  Carga un modelo previamente generado por keras, con formato .h5.
+#  @param nombre_modelo nombre del modelo .h5 que se desea cargar, sea tambien la ruta donde se ubica el modelo.
+#  @return regresa un objecto con el modelo de los pesos cargado.
 def cargar_modelo(nombre_modelo):
 
 
@@ -21,11 +24,19 @@ def cargar_modelo(nombre_modelo):
 
     return modelo_read
 
+#
+#  Compila un modelo previamente cargado para luego ser ejecutado.
+#  @param modelo objecto con el modelo previamente cargado.
+#  @return modelo objecto con el modelo compilado.
 def compiler_modelo(modelo):
 
     modelo.compile(loss='mean_squared_error',optimizer='adam',metrics=['binary_accuaracy'])
     print("Modelo Compilado")
     return modelo
+
+    #si el objecto no es pasado por referencia y en vez solo se instacia a la funcion sea
+    #model =compiler_modelo()
+    #puede que el modelo no se compile correctamente o no lo haga del todo.
 
 if __name__=="__main__":
     main()
