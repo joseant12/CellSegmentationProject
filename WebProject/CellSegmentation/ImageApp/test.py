a7e3fab4039391038305949572cc36a898d9357f
joseant12 <joseantonio122@gmail.com>
2018-09-15 21:57:10 -0600

    Comentarios a pruebas de Django

--- WebProject/CellSegmentation/ImageApp/test.py
+++ WebProject/CellSegmentation/ImageApp/test.py
@@ -8,6 +8,9 @@ from . import forms
 
 class TestImagenes(TestCase):
         def test_format(self):
+                """Función que evalua si el formato de una imagen se conserva
+                @return: resultado de prueba comparando si la imagen es formato png
+                """
                 coleccion_objeto = Coleccion()
                 image_objeto = Image()
                 image_objeto.image_File = SimpleUploadedFile(name='black.png', content=open('../../black.png', 'rb').read(), content_type='image/png')
@@ -15,6 +18,9 @@ class TestImagenes(TestCase):
                 self.assertEqual(image_objeto.load_image_function().format,'PNG')
 
         def test_add_photo(self):
+                """Función que evalua si la base de datos guarda satisfactoriamente la imagen
+                @return: resultado de prueba comparando si la colección de objetos tiene un tamaño de 1
+                """
                 coleccion_objeto = Coleccion()
                 coleccion_objeto.save()
                 image_objeto = Image()
@@ -24,6 +30,9 @@ class TestImagenes(TestCase):
                 self.assertEqual(Image.objects.count(), 1)
 
         def test_size(self):
+                """Función que evalua si el tamaño de la imagen guardada corresponde a la original
+                @return: resultado de prueba comparando si la imagen guardada tiene las mismas dimensiones.
+                """
                 coleccion_objeto = Coleccion()
                 image_objeto = Image()
                 image_objeto.image_File = SimpleUploadedFile(name='black.png', content=open('../../black.png', 'rb').read(), content_type='image/png')
@@ -31,6 +40,9 @@ class TestImagenes(TestCase):
                 self.assertEqual(image_objeto.load_image_function().size,(275,183))
 
         def test_form(self):
+                """Función que evalua si el formulario es valido
+                @return: retorna la condición en la cual se encuentra el formulario de colección.
+                """
                 form_data = {'titulo':'titulo','descripcion':'descripcion'}
                 form = ColeccionForm(data=form_data)
                 self.assertTrue(form.is_valid())
